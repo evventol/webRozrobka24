7d811bb311396ebaf34920d0203f7e350e4ff9cc
Denis <you@example.com>
2024-08-07 16:51:39 +0300

    js code

--- html_basics/denis/Forum/ts1.js
+++ html_basics/denis/Forum/ts1.js
@@ -1,4 +1,29 @@
-let rail = 12
-const red = 12.9
-let tyre = 34
-console.log("Stap")
+
+let car = {
+    name: "Fantom S+",
+    color: "blue",
+    width: 11 ,
+    leh: 25 ,
+    maxspeed: 330,
+    engine: "VW / AUDI / SKODA / SEAT  1.8 T"
+
+}
+console.log(car.color)
+let garanderr = [11,24,45,67,56,89,78,90,]
+let s = garanderr[7]
+console.log(s)
+
+let op = 22;
+if (op >=18) {
+    console.log("Go thtough");
+    
+} else {
+    console.log("yor a not 18 years");
+}
+
+const userinput = prompt("You name:","")
+if (userinput.length >0 ){
+    alert(userinput);
+} else {
+    alert("No name");
+}
