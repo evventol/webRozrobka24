1c376f0b75f13bd0b5e6be9cfbc14918381a654f
Nikita <you@example.com>
2024-08-07 16:50:16 +0300

    home

--- html_basics/matrix/nikita/gg.js
+++ html_basics/matrix/nikita/gg.js
@@ -1,4 +1,33 @@
-var myName =  "Нікіта"
-let age = "15"
-let type = "36"
-console.log ("Stap")
+
+let phone = {
+model: "Apple",
+color: "Black",
+procesor: "snepDragon",
+versionn: 18,
+ios: "ios",
+numder: 15,
+pr: "pro max",
+}
+console.log (phone.color);
+
+const ages = [18,20,15,14];
+let er = ages[0];
+console.log (ages)
+console.log (er)
+
+let score = 250
+
+if (score >= 260) {
+    console.log("Vidmina");
+} else if (score >= 240) {
+    console.log("Dobre");
+} else {
+    console.log("Sprobui she raz")
+}
+
+const userlnput = prompt("Vedite vashe imya:","");
+if (userlnput.length > 0){
+    alert(userlnput);
+} else {
+    alert("No name");
+}
