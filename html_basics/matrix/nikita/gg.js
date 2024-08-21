7376ee6cbe3e521a941c1cfdbe35f6454bd4d0bf
Nikita <you@example.com>
2024-08-21 16:56:50 +0300

    steam

--- html_basics/matrix/nikita/gg.js
+++ html_basics/matrix/nikita/gg.js
@@ -10,54 +10,54 @@ pr: "pro max",
 }
 console.log (phone.color);
 
-const ages = [18,20,15,14];
-let er = ages[0];
-console.log (ages)
-console.log (er)
+// const ages = [18,20,15,14];
+// let er = ages[0];
+// console.log (ages)
+// console.log (er)
 
-let score = 250
+// let score = 250
 
-if (score >= 260) {
-    console.log("Vidmina");
-} else if (score >= 240) {
-    console.log("Dobre");
-} else {
-    console.log("Sprobui she raz")
-}
-
-// const userlnput = prompt("Vedite vashe imya:","");
-// if (userlnput.length > 0){
-//     alert(userlnput);
+// if (score >= 260) {
+//     console.log("Vidmina");
+// } else if (score >= 240) {
+//     console.log("Dobre");
 // } else {
-//     alert("No name");
+//     console.log("Sprobui she raz")
 // }
 
-// for (let d = 0; d < 100; d++){
-//     console.log("Hello bro");
+// // const userlnput = prompt("Vedite vashe imya:","");
+// // if (userlnput.length > 0){
+// //     alert(userlnput);
+// // } else {
+// //     alert("No name");
+// // }
+
+// // for (let d = 0; d < 100; d++){
+// //     console.log("Hello bro");
+// // }
+
+
+// let a = prompt("Name"," ")
+// while(a === null || a.length === 0){
+//     alert(noname);
+//     a = prompt("a"," ");
+// }
+
+// let age = prompt("age"," ")
+// while(age === null || age.length === 0){
+//     alert(noname);
+//     age = prompt("age"," ");
 // }
 
 
-let a = prompt("Name"," ")
-while(a === null || a.length === 0){
-    alert(noname);
-    a = prompt("a"," ");
-}
+// function sayHello(name,age) {
+//     console.log("Privet, " + name,age + "!")
+// }
 
-let age = prompt("age"," ")
-while(age === null || age.length === 0){
-    alert(noname);
-    age = prompt("age"," ");
-}
+// sayHello(a,age)
 
+// const button = document.getElementById("myButton");
 
-function sayHello(name,age) {
-    console.log("Privet, " + name,age + "!")
-}
-
-sayHello(a,age)
-
-const button = document.getElementById("myButton");
-
-button.addEventListener("click",function() {
-    alert("btton clicked!");
-});
+// button.addEventListener("click",function() {
+//     alert("btton clicked!");
+// });
