85538a643560b12ac9c7e3b1fbe3fa05659ca249
Polina <yurchenko.polina1992@gmail.com>
2024-08-28 12:23:48 +0300

    My emotional state(JS code)

---
+++ html_basics/me/Vitalik/Vitalik.js
@@ -0,0 +1,32 @@
+document.getElementById("item1").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 1";
+});
+
+document.getElementById("item2").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 2";
+});
+
+document.getElementById("item3").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 3";
+});
+
+document.getElementById("item4").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 4";
+});
+
+document.getElementById("item5").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 5";
+});
+
+document.getElementById("item6").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 6";
+});
+
+document.getElementById("item7").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 7";
+});
+
+document.getElementById("item8").addEventListener("click", function()){
+    document.getElementById("output").innerText = "Hello 8";
+});
+
